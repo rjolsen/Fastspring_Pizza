f5fe96781f939b8bbc8b57048690f46049828dbe
r_olsen <r_olsen@q.com>
2018-11-12 20:21:42 -0700

    Update junit tests

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ Environment and Toolset:
 
 Comments:
 >Due to time constraints and not having a development environment setup I assumed the quickest would be to use Eclipse and HttpSerlets to do the rendering of HTML pages.  Also without any database installed I chose to update static objects in memory in order to add a remove items from a list of pizza toppings, In real life database tables would be better suited to contain the data items that can be added or removed by a user configured with authorized privileges.  JUnit test cases were created for all class Getter and Setter methods even though this is not necessary since they typically do not manipulate the data.
+
+Main URL:
+>http://localhost:8080/Fastspring_Pizza/
+
+Tracking URL:
+>http://localhost:8080/Fastspring_Pizza/TrackOrder
+
+Update Toppings URL:
+>http://localhost:8080/Fastspring_Pizza/UpdateToppings
+
+Manager URL:
+>http://localhost:8080/Fastspring_Pizza/Manager
--- test/com/fastspring/pizza/PendingOrdersTest.java
+++ test/com/fastspring/pizza/PendingOrdersTest.java
@@ -2,43 +2,70 @@ package com.fastspring.pizza;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
 class PendingOrdersTest {
 
+	User user = new User();
+	
 	@Test
 	void testGetNumOrders() {
-		fail("Not yet implemented");
+		assertEquals(0, PendingOrders.getNumOrders());
 	}
 
 	@Test
 	void testIncNumOrders() {
-		fail("Not yet implemented");
+		PendingOrders.incNumOrders();
+		assertEquals(1, PendingOrders.getNumOrders());
 	}
 
 	@Test
 	void testDecNumOrders() {
-		fail("Not yet implemented");
+		PendingOrders.decNumOrders();
+		assertEquals(0, PendingOrders.getNumOrders());
 	}
 
 	@Test
 	void testGetOrders() {
-		fail("Not yet implemented");
+		HashMap<Integer, User> orders = PendingOrders.getOrders();
+		assertEquals(orders, PendingOrders.getOrders());
 	}
 
 	@Test
 	void testInsertOrder() {
-		fail("Not yet implemented");
+		user.name = "Tom";
+		user.phoneNumber = 1231234;
+		user.address = "123 W 7th";
+		Order order = new Order();
+		order.orderId = 0;
+		order.state = 1;
+		user.setCurrentOrder(order);
+		PendingOrders.insertOrder(0, user);
+		assertEquals(PendingOrders.getOrder(0), user);
 	}
 
 	@Test
 	void testGetOrder() {
-		fail("Not yet implemented");
+		user.name = "Joe";
+		user.phoneNumber = 2225544;
+		user.address = "334 57th Place N";
+		PendingOrders.insertOrder(1, user);
+		assertEquals(user, PendingOrders.getOrder(1));
 	}
 
 	@Test
 	void testChangeOrder() {
-		fail("Not yet implemented");
+		user.name = "Tom";
+		user.phoneNumber = 1231234;
+		user.address = "123 W 7th";
+		Order order = new Order();
+		order.orderId = 0;
+		order.state = 1;
+		user.setCurrentOrder(order);
+		PendingOrders.insertOrder(0, user);
+		assertTrue(PendingOrders.changeOrder(0));
 	}
 
 }
